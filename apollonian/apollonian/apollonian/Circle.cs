fc12bd0ab6d4dfce87ac1e6476dbf8091deacbc3
Miron <98542071+Miron-Filimonov@users.noreply.github.com>
2022-02-12 21:46:53 +0500

    Добавил класс для отрисовки кругов

---
+++ apollonian/apollonian/apollonian/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SkiaSharp;
+using Xamarin.Forms;
+
+namespace apollonian
+{
+    class Circle
+    {
+        public Point Center;
+        public float Radius;
+
+        public Circle(float new_x, float new_y, float new_radius)
+        {
+            Center = new Point(new_x, new_y);
+            Radius = Math.Abs(new_radius);
+            Random randomGen = new Random();
+        }
+
+        public void Draw(SKCanvas canvas, SKColor color)
+        {
+            if (Radius > 0)
+            {
+                var circle = new SKPaint
+                {
+                    IsAntialias = true,
+                    Style = SKPaintStyle.Fill,
+                    Color = SKColors.Black
+                };
+                var circleFill = new SKPaint
+                {
+                    IsAntialias = true,
+                    Style = SKPaintStyle.Fill,
+                    Color = color
+                };
+                canvas.DrawCircle((float)Center.X, (float)Center.Y, Radius, circle);
+                canvas.DrawCircle((float)Center.X, (float)Center.Y, Radius, circleFill);
+            }
+        }
+    }
+}
